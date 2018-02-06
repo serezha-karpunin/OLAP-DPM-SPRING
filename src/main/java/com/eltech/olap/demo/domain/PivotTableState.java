c5791be6257512242f9b615f8edfc280e50762ac
Sergey <serezha.karpunin@gmail.com>
2018-02-07 00:01:15 +0400

    Added metadata field to result dto

--- src/main/java/com/eltech/olap/demo/domain/PivotTableState.java
+++ src/main/java/com/eltech/olap/demo/domain/PivotTableState.java
@@ -1,15 +1,18 @@
 package com.eltech.olap.demo.domain;
 
+import com.eltech.olap.demo.domain.meta.PivotMetadata;
 import com.eltech.olap.demo.domain.table.Table;
 
-public class QueryResult {
+public class PivotTableState {
 
     private Table table;
     private String query;
+    private PivotMetadata metadata;
 
-    public QueryResult(Table table, String query) {
+    public PivotTableState(Table table, String query, PivotMetadata metadata) {
         this.table = table;
         this.query = query;
+        this.metadata = metadata;
     }
 
     public String getQuery() {
@@ -26,5 +29,13 @@ public class QueryResult {
 
     public void setTable(Table table) {
         this.table = table;
+    }
+
+    public PivotMetadata getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(PivotMetadata metadata) {
+        this.metadata = metadata;
     }
 }
